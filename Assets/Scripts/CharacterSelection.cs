7944395dcdd1fa12334892fe8d7153152e765e0d
Caina de Jesus <cainajes@gmail.com>
2019-04-25 04:02:01 -0300

    arrumado a dinamica do personagem e adicionado o menu principal

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    private GameObject[] characterList;
+    private int index;
+
+    private void Start()
+    {
+        index = PlayerPrefs.GetInt("CharacterSelected");
+
+        characterList = new GameObject[transform.childCount];
+        
+        for(int i = 0; i < transform.childCount; i++)
+            characterList[i] = transform.GetChild(i).gameObject;
+
+        foreach (GameObject go in characterList)
+            go.SetActive(false);
+
+        if (characterList[index])
+            characterList[index].SetActive(true);
+        
+    }
+}
