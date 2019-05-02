1eb48a3444fe70ff801e19d108ca4bdbc2dea36f
Cainã de Jesus Marciano <cainajes@gmail.com>
2019-05-02 20:20:46 -0300

    adicionado o bullet system

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int speed = 10;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Player enemy = other.gameObject.GetComponent<Player>();
+
+        if (enemy != null)
+        {
+            enemy.DamageTaken(5);
+        }
+
+    }
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+    }    
+}
